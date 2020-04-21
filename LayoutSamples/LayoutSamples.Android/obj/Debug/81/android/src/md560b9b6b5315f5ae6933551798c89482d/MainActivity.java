a47c7161091fe71dc255a5f21c84d5a5d0620ecb
Friman Vilcapoma Flores <friman.vilcapoma@tecsup.edu.pe>
2020-04-20 21:03:20 -0500

    Primer commit

---
+++ LayoutSamples/LayoutSamples.Android/obj/Debug/81/android/src/md560b9b6b5315f5ae6933551798c89482d/MainActivity.java
@@ -0,0 +1,47 @@
+package md560b9b6b5315f5ae6933551798c89482d;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("LayoutSamples.Droid.MainActivity, LayoutSamples.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("LayoutSamples.Droid.MainActivity, LayoutSamples.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
